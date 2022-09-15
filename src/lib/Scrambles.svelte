d022cd3f602106b1b361eb9cac210e92e2b8cadb
Mano Ségransan <mano.segransan@protonmail.com>
2022-09-15 18:31:46 +0200

    Fix scramble font that was not monospace

--- src/lib/Scrambles.svelte
+++ src/lib/Scrambles.svelte
@@ -3,9 +3,11 @@
   import { scrambleSize } from "../stores/settings.js";
 </script>
 
+<div class="flex justify-center">
 <div
-  class="font-monospace text-center container"
+  class="font-mono"
   style="font-size: {$scrambleSize}px; white-space: pre-line;"
 >
   {$scrambleString}
+</div>
 </div>
